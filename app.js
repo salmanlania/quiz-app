5cddd4d1d3114b7ffa6fcafd24bf77ce7802e8d3
Salman Lania <78688636+salmanlania@users.noreply.github.com>
2024-12-03 09:50:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -32,110 +32,110 @@ const quizQuestions = [
     },
     answer: "Server-side JavaScript runtime",
   },
-  // {
-  //   id: 4,
-  //   question:
-  //     "Which language is used for building the front-end in the MERN stack?",
-  //   options: {
-  //     a: "Java",
-  //     b: "HTML",
-  //     c: "JavaScript",
-  //     d: "CSS"
-  //   },
-  //   answer: "JavaScript"
-  // },
-  // {
-  //   id: 5,
-  //   question: "What is Express.js used for in the MERN stack?",
-  //   options: {
-  //     a: "Database interaction",
-  //     b: "Building user interfaces",
-  //     c: "Routing and handling HTTP requests",
-  //     d: "Server-side logic"
-  //   },
-  //   answer: "Routing and handling HTTP requests"
-  // },
-  // {
-  //   id: 6,
-  //   question: "What is the purpose of MongoDB in the MERN stack?",
-  //   options: {
-  //     a: "Frontend development",
-  //     b: "Database management (NoSQL)",
-  //     c: "Routing",
-  //     d: "State management"
-  //   },
-  //   answer: "Database management (NoSQL)"
-  // },
-  // {
-  //   id: 7,
-  //   question: "What is the primary use of React in the MERN stack?",
-  //   options: {
-  //     a: "Server-side scripting",
-  //     b: "Building dynamic user interfaces",
-  //     c: "Database management",
-  //     d: "Networking"
-  //   },
-  //   answer: "Building dynamic user interfaces"
-  // },
-  // {
-  //   id: 8,
-  //   question: "Which of the following is NOT a common tool in the MERN stack?",
-  //   options: {
-  //     a: "MongoDB",
-  //     b: "Express",
-  //     c: "React",
-  //     d: "Ruby on Rails"
-  //   },
-  //   answer: "Ruby on Rails"
-  // },
-  // {
-  //   id: 9,
-  //   question:
-  //     "In the software industry, what is the purpose of Continuous Integration (CI)?",
-  //   options: {
-  //     a: "To automate deployment processes",
-  //     b: "To integrate new features into production manually",
-  //     c: "To ensure frequent code integration and testing",
-  //     d: "To monitor database performance"
-  //   },
-  //   answer: "To ensure frequent code integration and testing"
-  // },
-  // {
-  //   id: 10,
-  //   question: "What is a REST API?",
-  //   options: {
-  //     a: "A type of database management system",
-  //     b: "A set of conventions for web services",
-  //     c: "A programming language",
-  //     d: "A server-side framework"
-  //   },
-  //   answer: "A set of conventions for web services"
-  // }
+  {
+    id: 4,
+    question:
+      "Which language is used for building the front-end in the MERN stack?",
+    options: {
+      a: "Java",
+      b: "HTML",
+      c: "JavaScript",
+      d: "CSS"
+    },
+    answer: "JavaScript"
+  },
+  {
+    id: 5,
+    question: "What is Express.js used for in the MERN stack?",
+    options: {
+      a: "Database interaction",
+      b: "Building user interfaces",
+      c: "Routing and handling HTTP requests",
+      d: "Server-side logic"
+    },
+    answer: "Routing and handling HTTP requests"
+  },
+  {
+    id: 6,
+    question: "What is the purpose of MongoDB in the MERN stack?",
+    options: {
+      a: "Frontend development",
+      b: "Database management (NoSQL)",
+      c: "Routing",
+      d: "State management"
+    },
+    answer: "Database management (NoSQL)"
+  },
+  {
+    id: 7,
+    question: "What is the primary use of React in the MERN stack?",
+    options: {
+      a: "Server-side scripting",
+      b: "Building dynamic user interfaces",
+      c: "Database management",
+      d: "Networking"
+    },
+    answer: "Building dynamic user interfaces"
+  },
+  {
+    id: 8,
+    question: "Which of the following is NOT a common tool in the MERN stack?",
+    options: {
+      a: "MongoDB",
+      b: "Express",
+      c: "React",
+      d: "Ruby on Rails"
+    },
+    answer: "Ruby on Rails"
+  },
+  {
+    id: 9,
+    question:
+      "In the software industry, what is the purpose of Continuous Integration (CI)?",
+    options: {
+      a: "To automate deployment processes",
+      b: "To integrate new features into production manually",
+      c: "To ensure frequent code integration and testing",
+      d: "To monitor database performance"
+    },
+    answer: "To ensure frequent code integration and testing"
+  },
+  {
+    id: 10,
+    question: "What is a REST API?",
+    options: {
+      a: "A type of database management system",
+      b: "A set of conventions for web services",
+      c: "A programming language",
+      d: "A server-side framework"
+    },
+    answer: "A set of conventions for web services"
+  }
 ];
 
 console.log("quizQuestions length", quizQuestions.length);
-var quesContainer = document.getElementById("quesContainer");
-var question = document.getElementById("question");
-var optionsList = document.getElementById("optionsList");
-var nextBtn = document.getElementById("nextBtn");
-var resultCard = document.getElementById("resultCard");
-var correctAnswers = document.getElementById("correctAnswers");
-var wrongAnswers = document.getElementById("wrongAnswers");
-var score = document.getElementById("score");
-var percentage = document.getElementById("percentage");
-var resultStatus = document.getElementById("status");
-var indexNumber = 0;
-var correctAns = 0;
-var wrongAns = 0;
+let quesContainer = document.getElementById("quesContainer");
+let question = document.getElementById("question");
+let optionsList = document.getElementById("optionsList");
+let nextBtn = document.getElementById("nextBtn");
+let resultCard = document.getElementById("resultCard");
+let correctAnswers = document.getElementById("correctAnswers");
+let wrongAnswers = document.getElementById("wrongAnswers");
+let score = document.getElementById("score");
+let percentage = document.getElementById("percentage");
+let resultStatus = document.getElementById("status");
+let indexNumber = 0;
+let correctAns = 0;
+let wrongAns = 0;
 
 const handleQuestion = () => {
   nextBtn.disabled = false;
-  var questionPrint =
+  let questionPrint =
     indexNumber + 1 + ")" + " " + quizQuestions[indexNumber].question;
-  var optionPrint = quizQuestions[indexNumber].options;
+  let optionPrint = quizQuestions[indexNumber].options;
   question.innerHTML = questionPrint;
   optionsList.innerHTML = "";
-  for (var key in optionPrint) {
+  for (let key in optionPrint) {
     optionsList.innerHTML += `
     <form>
     <label>
@@ -173,7 +173,7 @@ const submission = () => {
   quesContainer.style.display = "none";
   resultCard.style.display = "block";
   score.innerHTML = correctAns;
-  var per = (correctAns / quizQuestions.length) * 100;
+  let per = (correctAns / quizQuestions.length) * 100;
   percentage.innerHTML = per;
   if (correctAns > 5 && correctAns <= 10) {
     resultStatus.innerHTML = "Pass";
@@ -183,13 +183,13 @@ const submission = () => {
 };
 
 const checkAnswer = (element) => {
-  var allOptions = optionsList.children;
-  // var optionClick = element
+  let allOptions = optionsList.children;
+  // let optionClick = element
   console.log("allOptions", allOptions);
   // return;
-  //   var optionClick = element.innerText || element.textContent;
-  var optionClick = element.value;
-  var answerOption = quizQuestions[indexNumber].answer;
+  //   let optionClick = element.innerText || element.textContent;
+  let optionClick = element.value;
+  let answerOption = quizQuestions[indexNumber].answer;
   console.log("answerOption", answerOption);
   // return;
   if (optionClick === answerOption) {
@@ -201,7 +201,7 @@ const checkAnswer = (element) => {
 
     // element.style.backgroundColor = "red";
 
-    for (var i = 0; i < allOptions.length; i++) {
+    for (let i = 0; i < allOptions.length; i++) {
       if (allOptions[i].innerHTML.toLowerCase() == answerOption) {
         // allOptions[i].style.backgroundColor = "red";
         console.log("allOptions[i]", allOptions[i]);
